0e0e877ef22bb3681b7a7c4eb33b9e0745d42355
amirmhmdzd92 <wackeramir@gmail.com>
2024-12-24 17:47:07 +0330

    Update python.py

--- python.py
+++ python.py
@@ -1,16 +1,17 @@
-pizza = 120
-felafel = 25
 from time import *
+#Use the time library
 print("hello")
 i = input("what your order?:")
 print("ok",i,"and?:")
 u = input("")
+#order registration
 if u == "none":
     print("ok pls wait 4min")
 else:
     print(i,"and",u,"pls wait 4min")
 sleep(4)
 print("sir sir sir! oh hi your order is ready")
+#Prepare the order
 y = input("cash or card?:")
 if y == "cash":
     print("ok",i,"and",u,"can be 125$")
@@ -20,3 +21,4 @@ else:
     print("ok",i,"and",u,"can be 125$")
     sleep(2)
     print("tnx nice to meet you")
+#Order payment
